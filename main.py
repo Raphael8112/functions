fdcc556e2b38bdc0830b265c1b65e8a74fc79d5f
Donald8112$ <donald.kintu13@gmail.com>
2024-12-13 16:23:36 +0300

    functions

---
+++ main.py
@@ -0,0 +1,5 @@
+# create a function that takes info from the user.
+def intro (name):#function-intro has 1 parameter-name
+    print("Hello",name)
+user_name=input("Enter your username:")
+intro(user_name) #function call
